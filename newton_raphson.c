afe3eaafd623e341245480185df432fc9d79c8e1
Hakizimana <hshaka2004@gmail.com>
2024-03-29 21:02:41 +0300

    update2

--- newton_raphson.c
+++ newton_raphson.c
@@ -1,30 +1,30 @@
 #include<stdio.h>
-#include<stdlib.h>
 #include<math.h>
-#define EPISLON 0.005
+
+double f(double x){
+    return pow(x,3)-pow(x,2)-1;//original function
+}
+double df(double x){
+    return (3*pow(x,2))-(2*x);//derivative of the function
+}
 // Newton Raphson Method function 
 double newton_raphson(double intial_approximate){
-    double intial_x = intial_approximate,final_x,error;
-    int number_of_iterations=0;
-    do
-    {
-        final_x = ((2*pow(intial_x,3)-(pow(intial_x,2)-1))/((3*pow(intial_x,2))-(2*intial_x)));
+    double intial_x = intial_approximate,final_x,error; int number_of_iterations=0;
+    do{//application of the Newton Raphson Formula
+        final_x = intial_x - (f(intial_x)/df(intial_x));
         error = fabs(final_x-intial_x);
-        if(error > EPISLON){
-        printf("x%d=%lf      x%d=%lf\n",(number_of_iterations),intial_x,(number_of_iterations+1),final_x);
+        //checking for convergence
+        if(error > 0.005){printf("x%d=%lf      x%d=%lf\n",(number_of_iterations),intial_x,(number_of_iterations+1),final_x);
         printf("Error = |%lf - %lf|=%lf \n\n",final_x, intial_x,error);}
-        else{
-        printf("x%d=%lf      x%d=%lf\n",(number_of_iterations),intial_x,(number_of_iterations+1),final_x);
+        else{printf("x%d=%lf      x%d=%lf\n",(number_of_iterations),intial_x,(number_of_iterations+1),final_x);
         printf("Error = |%lf - %lf|=%lf  < 0.005\n\n",final_x, intial_x,error);}
         intial_x = final_x;
         number_of_iterations++;
-    }while(error > EPISLON );
-
+    }while(error > 0.005 );
     printf("The number of iterations = %d",number_of_iterations);
     printf("\nThe final value of x is %lf",final_x);
     return final_x;
 }
-
 int main(void){
     double intial_approximate;
     printf("Enter the intial_approximation (Using 0.005 as error bound): ");
